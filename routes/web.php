e5c1e5dd96da93d76068929c313ccfc449670409
ahmed_alashii <you@example.com>
2023-05-26 16:17:28 +0300

    ..

--- routes/web.php
+++ routes/web.php
@@ -147,18 +147,18 @@ Route::group(['prefix' => 'manager/', 'as' => 'manager.'], function () {
     });
 });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('user');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('user');
 
-Route::get('/home/customer', [App\Http\Controllers\HomeController::class, 'customer'])->middleware('user', 'fireauth');
+// Route::get('/home/customer', [App\Http\Controllers\HomeController::class, 'customer'])->middleware('user', 'fireauth');
 
-Route::get('/email/verify', [App\Http\Controllers\Auth\ResetController::class, 'verify_email'])->name('verify')->middleware('fireauth');
+// Route::get('/email/verify', [App\Http\Controllers\Auth\ResetController::class, 'verify_email'])->name('verify')->middleware('fireauth');
 
-Route::post('login/{provider}/callback', 'Auth\LoginController@handleCallback');
+// Route::post('login/{provider}/callback', 'Auth\LoginController@handleCallback');
 
-Route::resource('/home/profile', App\Http\Controllers\Auth\ProfileController::class)->middleware('user', 'fireauth');
+// Route::resource('/home/profile', App\Http\Controllers\Auth\ProfileController::class)->middleware('user', 'fireauth');
 
-Route::resource('/password/reset', App\Http\Controllers\Auth\ResetController::class);
+// Route::resource('/password/reset', App\Http\Controllers\Auth\ResetController::class);
 
-Route::resource('/img', App\Http\Controllers\ImageController::class);
+// Route::resource('/img', App\Http\Controllers\ImageController::class);
